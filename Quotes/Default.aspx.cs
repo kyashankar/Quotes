aae4678ea67851c970033d85267c58f7db7a519f
dev01 <dev01@SQL01>
2024-11-13 12:35:24 +0530

    Add project files.

---
+++ Quotes/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace Quotes
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //QuoteRepository quoteRepository = new QuoteRepository();
+            
+            repeaterQuotes.DataSource = GetQuotess();
+            repeaterQuotes.DataBind();
+        }
+
+        public List<string> GetQuotess()
+        {
+            List<string> quotes = new List<string>();
+
+            quotes.Add("Creativity is intelligence having fun");
+            quotes.Add("Champions keep playing until they get it right");
+            quotes.Add("The best time to lant a tree was 20 years ago. The second best time is now");
+            quotes.Add("The only person you are destined to becoe is the person you decide to be ");
+            quotes.Add("Believe you can and yu're halfway there");
+
+            return quotes;
+        }
+    }
+}
